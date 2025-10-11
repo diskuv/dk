51e08e9442803df20ca4ba1942987d9ee95d890c
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2025-10-10 22:16:28 -0700

    Move to etc/dk/v
    "v/" (new) is for values. "d/" (already exists) is for distributions.
    
    + Enable Harden Runner
    + Change default -I in mlfront-shell to etc/dk/v

--- mlfront-shell.cmd
+++ mlfront-shell.cmd
@@ -146,7 +146,7 @@ SET "DK_QUIET="
 SET "_DK_PATH="
 SET "_XCOPY_SWITCHES="
 REM     Then run it
-"%DK_EXE%" %_DKEXE_OPTIONS% -I "%DK_PROJECT_DIR%\pkgs\include" %*
+"%DK_EXE%" %_DKEXE_OPTIONS% -I "%DK_PROJECT_DIR%\etc\dk\v" %*
 EXIT /B %ERRORLEVEL%
 
 REM ------ SUBROUTINE [downloadFile]
