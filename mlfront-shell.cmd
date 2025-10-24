17e0d202a385b71522a413d4881a6a61ff8d5868
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2025-10-24 01:20:44 -0700

    mlfront-shell 2.4.2.27
    2.4.2.25 had no binaries.

--- mlfront-shell.cmd
+++ mlfront-shell.cmd
@@ -41,14 +41,14 @@ SET DKCODER_PWD=%CD%
 
 REM Update within dksdk-coder:
 REM   f_mlfrontshell() { ver=$1; install -d build; for i in darwin_arm64 darwin_x86_64 linux_x86 linux_x86_64 windows_x86_64 windows_x86; do extexe=; case $i in windows_*) extexe=.exe ;; esac; curl -Lo "build/mlfrontshell-$i" "https://gitlab.com/api/v4/projects/60486861/packages/generic/shell/$ver/mlfront-shell-$i$extexe"; done }
-REM   f_mlfrontshell 2.4.2.25
+REM   f_mlfrontshell 2.4.2.27
 REM   shasum -a 256 build/mlfrontshell-* | awk 'BEGIN{FS="[ /-]"} {printf "SET DK_CKSUM_%s=%s\n", toupper($5), $1}' | sort
 REM
 REM   Empty value if the architecture is not supported.
 REM -------------------------------------
-SET DK_VER=2.4.2.25
-SET DK_CKSUM_WINDOWS_X86_64=9491d4737000e80bcbdd7a39e9dc13c2178ff865beff7d800d6159bfc395e8fa
-SET DK_CKSUM_WINDOWS_X86=9491d4737000e80bcbdd7a39e9dc13c2178ff865beff7d800d6159bfc395e8fa
+SET DK_VER=2.4.2.27
+SET DK_CKSUM_WINDOWS_X86_64=81d512fd0e32b840646f185a24ee492be53f4ddd44c21af78241f9bacf1bd1f2
+SET DK_CKSUM_WINDOWS_X86=a134108ac7df14cd55dbd1a8ac19a59addda264e4872e14ec8ca79936de71ceb
 
 REM --------- Quiet Detection ---------
 SET DK_QUIET=0
