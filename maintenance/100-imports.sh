9521d823b4af010b22a4cebfbe07b5339bad9f9d
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2025-12-19 15:47:29 -0800

    Import L2 2.4.202512170003
    + bugfix: dksrc not dk0 in 100-imports.sh
    + Use --trial when importing

--- maintenance/100-imports.sh
+++ maintenance/100-imports.sh
@@ -6,8 +6,8 @@
 
 set -euf
 
-rm -rf dk0/
+rm -rf dksrc/
 git clone https://github.com/diskuv/dk.git dksrc
 
 # Import GitHub-attested artifacts
-dksrc/dk0 import-github-l2 --repo diskuv/dk --tag 2.4.202512120000 --outdir etc/dk/i/
+dksrc/dk0 --trial import-github-l2 --repo diskuv/dk --tag 2.4.202512170003 --outdir etc/dk/i/
